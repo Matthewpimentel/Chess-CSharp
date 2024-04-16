674882a9569c4d0b1c5f7253f7b2fdbb809c35f9
Matthew Pimentel <matthewpimentel@hotmail.com>
2024-04-16 13:16:39 -0400

    Game can now end with checkmate/stalemate

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -7,6 +7,8 @@ namespace ChessLogic
         public Board Board { get; }
         public Player CurrentPlayer { get; private set; }
 
+        public Result Result { get; private set; } = null;
+
         public GameState(Player player, Board board)
         {
             CurrentPlayer = player;
@@ -29,6 +31,38 @@ namespace ChessLogic
         {
             move.Execute(Board);
             CurrentPlayer = CurrentPlayer.Opponent();
+            CheckForGameOver();
+        }
+
+        public IEnumerable<Move> AllLegalMovesFor(Player player)
+        {
+            IEnumerable<Move> moveCanidates = Board.PiecePositionsFor(player).SelectMany(pos =>
+            {
+                Piece piece = Board[pos];
+                return piece.GetMoves(pos, Board);
+            });
+
+            return moveCanidates.Where(Move => Move.IsLegal(Board));
+        }
+
+        private void CheckForGameOver()
+        {
+            if (!AllLegalMovesFor(CurrentPlayer).Any())
+            {
+                if (Board.IsInCheck(CurrentPlayer))
+                {
+                    Result = Result.Win(CurrentPlayer.Opponent());
+                }
+                else
+                {
+                    Result = Result.Draw(EndReason.Stalemate);
+                }
+            }
+        }
+
+        public bool IsGameOver()
+        {
+            return Result != null;
         }
     }
 }
