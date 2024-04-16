fbda08f57d691192bb86dc29b137b388db7e23ec
Matthew Pimentel <matthewpimentel@hotmail.com>
2024-04-16 12:10:01 -0400

    Moves are now generated for rook, bishop and queen

---
+++ ChessLogic/Moves/NormalMove.cs
@@ -0,0 +1,30 @@
+﻿using ChessLogic.Pieces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class NormalMove : Move
+    {
+        public override MoveType Type => MoveType.Normal;
+        public override Position FromPos { get;}
+        public override Position ToPos { get;}
+
+        public NormalMove(Position from, Position to)
+        {
+            FromPos = from;
+            ToPos = to;
+        }
+
+        public override void Execture(Board board)
+        {
+            Piece piece = board[FromPos];
+            board[ToPos] = piece;
+            board[FromPos] = null;
+            piece.HasMoved = true;
+        }
+    }
+}
--- ChessLogic/Pieces/Queen.cs
+++ ChessLogic/Pieces/Queen.cs
@@ -7,6 +7,18 @@ namespace ChessLogic
         public override PieceType Type => PieceType.Queen;
         public override Player Color { get; }
 
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.North,
+            Direction.South,
+            Direction.East,
+            Direction.West,
+            Direction.NorthWest,
+            Direction.NorthEast,
+            Direction.SouthWest,
+            Direction.SouthEast,
+        };
+
         public Queen(Player color)
         {
             Color = color;
@@ -17,6 +29,11 @@ namespace ChessLogic
             Queen copy = new Queen(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositionsInDirs(from, board, dirs).Select(to => new NormalMove(from, to));
         }
     }
 }
